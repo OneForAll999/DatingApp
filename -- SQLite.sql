6a9c939e4a8d14ceddf0212a365c4625b31aea06
OneForAll999 <kapsik99@gmail.com>
2022-06-22 16:40:08 +0200

    End of section 2

---
+++ -- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Filip");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Monika");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Szymon");
